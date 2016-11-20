1e4f6b3ffda5114cf40e598d1b3fcfb23d986424
rain1024 <brother.rain.1024@gmail.com>
2016-11-20 18:29:02 +0700

    add docker

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-## Dockerizing Flask With Compose and Machine - From Localhost to the Cloud
+# Python Challenge 
 
-Featuring:
+# Powered By 
 
-- Docker v1.9.0
-- Docker Compose v1.5.0
-- Docker Machine v0.5.0
+* docker
+* 
 
-**Check out the awesome blog post here > https://realpython.com/blog/python/dockerizing-flask-with-compose-and-machine-from-localhost-to-the-cloud/**
-
-Cheers!
--- web/app.py
+++ web/app.py
@@ -1,30 +1,13 @@
-# app.py
-
-
 from flask import Flask
-from flask import request, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
-from config import BaseConfig
-
+import os
 
 app = Flask(__name__)
-app.config.from_object(BaseConfig)
-db = SQLAlchemy(app)
 
 
-from models import *
+@app.route('/')
+def hello():
+    return 'Hello World!'
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        text = request.form['text']
-        post = Post(text)
-        db.session.add(post)
-        db.session.commit()
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('index.html', posts=posts)
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
