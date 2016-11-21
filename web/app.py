e341282159a1b38c3aa3d80670648f0ea4e742ff
Vu Anh <brother.rain.1024@gmail.com>
2016-11-21 10:31:49 +0700

    add levels

--- web/app.py
+++ web/app.py
@@ -16,22 +16,54 @@ def hello():
 def level1():
     return render_template("level1.html")
 
+@app.route('/level2')
+def level2():
+    return render_template("level2.html")
+
+
 @app.route('/badge')
 def badge():
     return render_template("badge.html")
 
-
-@app.route("/execute", methods=["POST"])
+levels_flow = {
+    "level1": "level2",
+    "level2": "graduation"
+}
+@app.route("/submit", methods=["POST"])
 def execute():
     try:
+        problem = request.json['problem']
         code = request.json['code']
-        with capture_output() as c:
-            exec code
-        expected = open("static/levels/1/ouput01.txt")
-        actual = c.stdout
-        result = {"b": "1"}
+        files = os.listdir("static/levels/" + problem)
+        results = []
+        for i in range(len(files) / 2):
+            input_file = "static/levels/%s/input%02d.txt" % (problem, i)
+            output_file = "static/levels/%s/output%02d.txt" % (problem, i)
+            expected = open(output_file).read()
+            with capture_output() as c:
+                exec code
+            actual = c.stdout
+            result = expected == actual
+            results.append(result)
+        results = all(results)
+        if results:
+            if levels_flow[problem] == "graduation":
+                result = {
+                    "graduation": "graduation"
+                }
+            else:
+                result = {
+                    "success": "success",
+                    "next": "level2"
+                }
+        else:
+            result = {
+                "error": "Wrong Answer"
+            }
     except SyntaxError, e:
-        result = {"error": "syntax error"}
+        result = {"error": "Syntax Error"}
+    except Exception, e:
+        result = {"error": str(e)}
     finally:
         return flask.jsonify(**result)
 
---
+++ web/static/js/configuration.js
@@ -0,0 +1,3 @@
+CONFIGURATION = {
+  SERVER: "http://127.0.0.1:5000"
+};
